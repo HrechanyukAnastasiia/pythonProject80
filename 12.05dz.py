8f046d090937ef16476698429d9549b53f4af4e8
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-12 17:22:40 +0300

    друге завдання

---
+++ 12.05dz.py
@@ -0,0 +1,14 @@
+#2
+def retur(func):
+    def wrapper(*args, **kwargs):
+        x = func(*args, **kwargs) + 100
+        print(x)
+    return wrapper
+@retur
+def dobutok(x):
+    res = 1
+    for args in x:
+        res *= args
+    return res
+lst = [1, 2, 5]
+dobutok(lst)
